dc1704b41ce538e67d9ca42308153330bd2541df
Alfred Woo <joona0825@digutsoft.com>
2015-04-13 09:14:41 +0900

    Car, CarTest class added

---
+++ JavaPractice/src/org/dimigo/oop/Car.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * org.dimigo.oop >> Car
+ *
+ * @author joona0825
+ */
+public class Car {
+	private String company, model, color;
+	private int maxSpeed, price;
+
+	public String getCompany() {
+		return company;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setCompany(String arg1) {
+		company = arg1;
+	}
+	
+	public void setModel(String arg1) {
+		model = arg1;
+	}
+	
+	public void setColor(String arg1) {
+		color = arg1;
+	}
+	
+	public void setMaxSpeed(int arg1) {
+		maxSpeed = arg1;
+	}
+	
+	public void setPrice(int arg1) {
+		price = arg1;
+	}
+}
