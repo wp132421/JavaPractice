97f7e29fd297aa2be2b4a68b573bbac0d3bc8034
Alfred Woo <joona0825@digutsoft.com>
2015-04-14 15:33:51 +0900

    Overloading!

---
+++ JavaPractice/src/org/dimigo/oop/Car3.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * org.dimigo.oop >> Car
+ *
+ * @author joona0825
+ */
+public class Car3 {
+	private String company, model, color;
+	private int maxSpeed, price;
+	
+	public Car3(String company, String model, String color, int maxSpeed, int price) {
+		this.company = company;
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+		this.price = price;
+	}
+	
+	public Car3(String company, String model, String color, int maxSpeed) {
+		this(company, model, color, maxSpeed, 0);
+	}
+	
+	public Car3(String company, String model, String color) {
+		this(company, model, color, 0, 0);
+	}
+
+	public String getCompany() {
+		return company;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+}
--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -32,10 +32,13 @@ public class CarTest {
 //		car3.setMaxSpeed(200);
 //		car3.setPrice(38000000);
 		
-
-		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
-		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
-		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+//		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
+//		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+//		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+		
+		Car3 car1 = new Car3("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car3 car2 = new Car3("기아자동차", "K7", "흰색", 246);
+		Car3 car3 = new Car3("삼성자동차", "SM7", "회색");
 		
 		System.out.println("<< 자동차 목록 >>");
 		System.out.println("제조사명: " + car1.getCompany());
