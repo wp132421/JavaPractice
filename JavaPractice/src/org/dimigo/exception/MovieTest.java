35be6b3c10530b81dc4c68de033f477e0e52c0c2
Alfred Woo <joona0825@digutsoft.com>
2015-09-22 14:32:56 +0900

    ver2 (parent: 1718a12d88ff18e51657b83d5a011411dccc0fd0)

--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -11,20 +11,20 @@ package org.dimigo.exception;
 public class MovieTest {
 	
 	public static void main(String[] args) {
-		try {
-			Movie[] movies = {
-					new Movie("앤트맨", 12),
-					new Movie("사도", 12),
-					new Movie("베테랑", 15)
-			};
-			
-			int age = 13;
-			for(Movie movie : movies) {
+		Movie[] movies = {
+				new Movie("앤트맨", 12),
+				new Movie("사도", 12),
+				new Movie("베테랑", 15)
+		};
+		
+		int age = 13;
+		for(Movie movie : movies) {
+			try {
 				buyTicket(movie, age);
+			} catch(Exception e) {
+				System.out.println(e.getMessage());
 			}
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}		
+		}
 	}
 	
 	private static void buyTicket(Movie movie, int age) throws AgeCheckException {
